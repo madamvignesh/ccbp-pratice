f8e717ab93f088a5350bb5a6c584118504940b7e
madamvignesh <madamvignesh@gmial.com>
2024-03-19 18:39:27 +0000

    Test Case 3: failed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const addDays = require("date-fns/addDays")
+const addDays = require('date-fns/addDays')
 
-module.exports=function(days=3){
-    const newDate = addDate(new Date(2020,7,20),days);
-    return (`${newDate.getDate()}-${newDate.getmonth()+1}-${newDate.getFullYear()}`)
+module.exports = function (days = 3) {
+  const newDate = addDate(new Date(2020, 7, 20), days)
+  return `${newDate.getDate()}-${
+    newDate.getmonth() + 1
+  }-${newDate.getFullYear()}`
 }
