aff7fc30a2e7c35abb816ff56acaa7bfa3d2dbfd
madamvignesh <madamvignesh@gmail.com>
2024-03-19 18:14:17 +0000

    The Function should return the date after x days from 22nd August 2020

---
+++ index.js
@@ -0,0 +1,6 @@
+const addDays = require("date-fns/addDays")
+
+module.exports=function(days=3){
+    const newDate = addDate(new Date(2020,7,20),days);
+    return (`${newDate.getDate()}-${newDate.getmonth()+1}-${newDate.getFullYear()}`)
+}
